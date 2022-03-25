090b2491e4347b78e0a1840a3cc53463c7910370
peterdesmet <peter.desmet@inbo.be>
2022-03-25 11:28:14 +0100

    Start a dwc_occurrence.sql

---
+++ sql/dwc_occurrence.sql
@@ -0,0 +1,12 @@
+/*
+Created by Peter Desmet (INBO)
+*/
+SELECT
+  NULL                                  AS eventID,
+-- OCCURRENCE
+  NULL                                  AS occurrenceID,
+  'present'                             AS occurrenceStatus
+-- IDENTIFICATION
+-- TAXON
+FROM
+  data
